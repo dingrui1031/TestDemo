7dec645abcd5389e7e0d36c74f04d2446b4e863c
dingrui <dingrui@greatld.com>
2022-08-17 18:14:36 +0800

    test new branch

--- app/src/main/java/com/example/testdemo/MainActivity.java
+++ app/src/main/java/com/example/testdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -38,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
     if (!imm.isActive()) {
       imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
     }
+  }
+
+  public void test() {
+    Log.e("tag", "test");
   }
 
 }
